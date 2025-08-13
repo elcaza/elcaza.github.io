1c172ba02e5ad432dd3820c64eaae91674adff31
El Capitán <heneverloses@gmail.com>
2025-08-13 16:08:58 -0600

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 // https://astro.build/config
 export default defineConfig({
 	site: "https://elcaza.github.io/",
-	base: "elcaza.github.io/",
+	base: "",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
